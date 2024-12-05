869a1018c7d3a707e5f029da2d96d2a4209a10b9
Samantha AC <samantha.avilac99@gmail.com>
2024-12-05 17:16:39 -0500

    cambios

--- app/src/main/java/com/boceto/inventario/screens/FormScreen.kt
+++ app/src/main/java/com/boceto/inventario/screens/FormScreen.kt
@@ -1,5 +1,7 @@
 package com.boceto.inventario.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
@@ -31,6 +34,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -95,7 +99,14 @@ fun FormList(navHostController: NavHostController) {
     ) {
         Text(text = "Bodega")
         Spacer(modifier = Modifier.height(10.dp))
-        Box {
+        Box (
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(8.dp))
+                .background(Color.White)
+                .border(1.dp, color = Color(0xFFE2DAD6), shape = RoundedCornerShape(8.dp))
+                .padding(4.dp)
+        ){
             Text(
                 text = if (bodega.isEmpty()) "Selecciona una bodega" else bodega,
                 modifier = Modifier
